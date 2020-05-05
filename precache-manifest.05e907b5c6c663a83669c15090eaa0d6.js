e3601f0f99b33b9d4c7dead69515e9b6858e1154
hhkp <s.volodymyr82@gmail.com>
2020-05-06 00:05:16 +0300

    Updates

---
+++ precache-manifest.05e907b5c6c663a83669c15090eaa0d6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15770284a0bb6cdb3fdba82ddcf4b2d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964bbb677891d967a825",
+    "url": "/static/css/main.c16663a8.chunk.css"
+  },
+  {
+    "revision": "28ff022e16bc27269757",
+    "url": "/static/js/2.fb6c0ef6.chunk.js"
+  },
+  {
+    "revision": "10017bfb08c3f52cf1c70a46d09c7c96",
+    "url": "/static/js/2.fb6c0ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964bbb677891d967a825",
+    "url": "/static/js/main.b9059be3.chunk.js"
+  },
+  {
+    "revision": "a555820f312f800a24a8",
+    "url": "/static/js/runtime-main.e0d361b0.js"
+  },
+  {
+    "revision": "cee5a5120b2a90790f349959c186d4c8",
+    "url": "/static/media/ali-yahya-7_AZi5Fe-rU-unsplash.cee5a512.png"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "515a70d366d00647c88425c547e29696",
+    "url": "/static/media/michal-parzuchowski-ItaV89TNkks-unsplash.515a70d3.png"
+  },
+  {
+    "revision": "b23de1c2ecd60bb9f8d5f55fe850dc2f",
+    "url": "/static/media/shane-rounce-5f4xGaQnM1E-unsplash.b23de1c2.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/static/media/up-arrow-circle.d6c4e78d.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
